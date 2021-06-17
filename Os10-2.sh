9f4c55aea85feed10c6071a24c67a9f7f6915e35
ahmad <59280503+ahmadhasheminajafi@users.noreply.github.com>
2021-06-17 02:38:59 -0700

    Add files via upload

---
+++ Os10-2.sh
@@ -0,0 +1,8 @@
+  
+#!/bin/bash
+
+for iterator in {1..100}
+
+do
+	mkdir user$iterator
+don
---
+++ Os10-3.sh
@@ -0,0 +1,14 @@
+ 
+#!/bin/bash
+
+read a
+cd a
+counter=1
+for file in *.jpg;
+do
+mv $file img $(++counter).jpg
+done
+for file in *.png;
+do 
+mv $file img $ (++counter).png
+done
